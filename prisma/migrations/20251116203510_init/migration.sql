a18d194ea93a27eb08d1528e96a1dd799f3d54d5
ahmedhalac <ahmedhalac@outlook.com>
2025-11-16 21:41:51 +0100

    Make dist file

--- prisma/migrations/20251116203510_init/migration.sql
+++ prisma/migrations/20251116203510_init/migration.sql
@@ -2,8 +2,9 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
