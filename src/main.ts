5c34c7c92a433cd89000eb4be4713ed7f62d54a8
ahmedhalac <ahmedhalac@outlook.com>
2025-11-24 23:21:16 +0100

    Added CORS restrictions

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { PrismaService } from './prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   // enableShutdownHooks allows Prisma to listen to NestJS shutdown events and clean up connections.
   // Without this, Prisma may leave hanging connections if you stop the app with CTRL+C
   app.enableShutdownHooks();
